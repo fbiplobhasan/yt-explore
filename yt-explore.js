df486d899786e60f4aca1bab5d98ae68f2d0ba43
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-05 07:32:50 +0600

    add stack

--- yt-explore.js
+++ yt-explore.js
@@ -1,42 +1,64 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.naxt = null;
-    }
-}
+// class Node {
+//     constructor(value) {
+//         this.value = value;
+//         this.naxt = null;
+//     }
+// }
 
-class Queue {
+// class Queue {
+//     constructor() {
+//         this.first = null
+//         this.last = null
+//         this.length = 0
+//     }
+//     enqueue(value) {
+//         const newPerson = new Node(value);
+//         if (!this.first) {
+//             this.first = newPerson;
+//             this.last = newPerson;
+//         } else {
+//             this.last.next = newPerson;
+//             this.last = newPerson;
+//         }
+//         this.length++;
+//         return this;
+//     }
+//     dequeue() {
+
+//         if (!this.first) {
+//             return null;
+//         }
+
+//         if (this.first === this.last) {
+//             this.last = null
+//         }
+
+//         this.first = this.first.next;
+//         this.length--;
+//         return this;
+//     }
+// }
+
+// let myQueue = new Queue();
+
+// console.log(myQueue.enqueue("rakib"));
+// console.log(myQueue.enqueue("hasib"));
+// console.log(myQueue.dequeue());
+// console.log(myQueue.enqueue("rafi"));
+// console.log(myQueue.dequeue());
+// console.log(myQueue.dequeue());
+
+class Stack {
     constructor() {
-        this.first = null
-        this.last = null
-        this.length = 0
-    }
-    enqueue(value) {
-        const newPerson = new Node(value);
-        if (!this.first) {
-            this.first = newPerson;
-            this.last = newPerson;
-        } else {
-            this.last.next = newPerson;
-            this.last = newPerson;
-        }
-        this.length++;
-        return this;
-    }
-    dequeue() {
+        this.arr = [];
 
-        if (!this.first) {
-            return undefined
-        }
-        this.first = this.first.next;
-        this.length--;
-        return this;
+    }
+    push(value) {
+        this.arr.push(value)
+        return this.arr;
     }
 }
 
-let myQueue = new Queue();
-
-console.log(myQueue.enqueue("rafi"));
-console.log(myQueue.enqueue("rakib"));
-console.log(myQueue.enqueue("hasib"));
-console.log(myQueue.dequeue());
+let stack = new Stack();
+console.log(stack.push(50));
+console.log(stack.push(500));
